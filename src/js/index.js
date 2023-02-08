9a05009f06c2dd6927fff718e1e2afcad0ac74a0
Letícia Lins <leticialins.rj@gmail.com>
2023-02-07 22:17:38 -0300

    trocar fonte, incluir recordes (css e js)

--- src/js/index.js
+++ src/js/index.js
@@ -3,46 +3,92 @@ var cartoes = document.querySelectorAll(".cartao");
 var existeCartaoVirado = false;
 var primeiroCartao, segundoCartao;
 
+var resultadosJogadores = [];
+
+var itensStorage = localStorage.getItem("resultado");
+
+if (itensStorage){
+  var resultadosExistentes = JSON.parse(itensStorage);
+}else{
+  var resultadosExistentes = [];
+}
+
+console.log(resultadosJogadores);
+
 var placar = 0;
 var tempo = 0;
 
 var intervalo = setInterval(function(){
   if (tempo == 15){
     clearInterval(intervalo);
-    cartoes.forEach(function(cartao){
-      cartao.addEventListener("click", virarCartao);
+    alert("Tempo esgotado!");
+    var nome = prompt("Digite o seu nome:");
+   
+    var resultado ={
+      nomeJogador: nome,
+      pontuacao: placar,
+    };
+
+    resultadosExistentes.push(resultado);
+
+    localStorage.setItem("resultado", JSON.stringify(resultadosExistentes));
+
+    document.getElementById("recordes").style.display = "block";
+
+    var tabela = document.getElementById("resultados");
+
+    resultadosExistentes.sort().forEach(function (resultado){
+      var tr = document.createElement("tr");
+      var tdNome = document.createElement("td");
+      tdNome.innerHTML = resultado.nomeJogador;
+      var tdPontuacao = document.createElement("td");
+      tdPontuacao.innerHTML = resultado.pontuacao;
+      tr.appendChild(tdNome);
+      tr.appendChild(tdPontuacao);
+      tabela.appendChild(tr);
     });
-      return;
+
+    cartoes.forEach(function (cartao){
+      cartao.removeEventListener("click", virarCartao);
+    });
+    return;
   }
-  
   tempo = tempo + 1;
+
   document.getElementById("tempo").innerHTML = tempo;
 }, 1000);
 
-
-
 function virarCartao(){
-    this.classList.add("virado");
+  this.classList.add("virado");
 
-    if(existeCartaoVirado == false){
-        existeCartaoVirado = true;
-        primeiroCartao = this;
+    if (existeCartaoVirado == false){
+      existeCartaoVirado = true;
+      primeiroCartao = this;
     }else{
-          segundoCartao = this;
-          existeCartaoVirado = false;
+      segundoCartao = this;
+      existeCartaoVirado = false;
 
-          if(primeiroCartao.dataset.info === segundoCartao.dataset.info) {
-            primeiroCartao.removeEventListener("click", virarCartao);
-            segundoCartao.removeEventListener("click", virarCartao);
-          }else{
-            setTimeout(function() {
-                primeiroCartao.classList.remove("virado");
-                segundoCartao.classList.remove("virado");
+        if (primeiroCartao.dataset.info === segundoCartao.dataset.info){
+          primeiroCartao.removeEventListener("click", virarCartao);
+          segundoCartao.removeEventListener("click", virarCartao);
+          placar = placar + 1;
+          document.getElementById("resultado").innerHTML = placar;
+        }else{
+          cartoes.forEach(function (cartao){
+            cartao.removeEventListener("click", virarCartao);
+          });
+            setTimeout(function(){
+              cartoes.forEach(function (cartao){
+                cartao.addEventListener("click", virarCartao);
+              });
+
+              primeiroCartao.classList.remove("virado");
+              segundoCartao.classList.remove("virado");
             }, 250);
           }
-        }
+      }
 }
 
 cartoes.forEach(function(cartao){
-    cartao.addEventListener("click",virarCartao);
+  cartao.addEventListener("click", virarCartao);
 });
